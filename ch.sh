c0a7316230b0bcaf7e3af6cd16ae08ba328f555c
rajdip34 <rajdipmondal34@gmail.com>
2019-02-04 16:47:39 +0530

    ngrock

---
+++ ch.sh
@@ -0,0 +1,2 @@
+sudo chmod +x /home/uianon/code/js/fast.js
+node /home/uianon/code/js/fast.js
---
+++ fast.sh
@@ -0,0 +1,7 @@
+export red=$'\e[0;91m'
+export green=$'\e[0;92m'
+export blue=$'\e[0;94m'
+export white=$'\e[0;97m'
+
+
+printf "${red} hello ${white} world ${blue} i'm Rajdip\\n"
